9a9b2184b0c8e5f2c6f4eac2ebc319ee74080457
LeonLin <expert41618@gmail.com>
2019-07-20 16:15:23 +0800

    Part1.
    -Build Retrofit request to OpenWeatherMap
    -Create Fragment display system
    -Get Device's location
    -Design UI for MainActivity
    
    libraries
    Common
    API Key
    RetrofitClient
    Interface
    Models (jsonEditor,json2sharp)(Coord.java @Override toString)
    MainActivity.xml
    MainActivity.java
    ViewPagerAdapter
    TodayWeatherFragment

---
+++ app/src/main/java/com/example/leo/weatherapp/Common/Common.java
@@ -0,0 +1,8 @@
+package com.example.leo.weatherapp.Common;
+
+import android.location.Location;
+
+public class Common {
+    public static final String APP_ID = "29c0749ccc6d8b61efa3b28daf33120b";
+    public static Location current_location = null;
+}
---
+++ app/src/main/java/com/example/leo/weatherapp/Retrofit/IOpenWeatherMap.java
@@ -0,0 +1,15 @@
+package com.example.leo.weatherapp.Retrofit;
+
+import com.example.leo.weatherapp.Model.WeatherResult;
+
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface IOpenWeatherMap {
+    @GET("weather")
+    Observable<WeatherResult> getWeatherByLating(@Query("lat") String lat,
+                                                 @Query("lon") String lon,
+                                                 @Query("appid") String appid,
+                                                 @Query("units") String unit);
+}
